9b9626833745005c0fd28f1dc8123ea3c69f72bd
Rasmus Thomsen <rasmus.thomsen@protonmail.com>
2018-11-02 23:39:36 +0100

    lib: cargo-fmt

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,8 @@
 extern crate reqwest;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_json;
 extern crate serde;
+extern crate serde_json;
 #[macro_use]
 extern crate failure;
 #[macro_use]
